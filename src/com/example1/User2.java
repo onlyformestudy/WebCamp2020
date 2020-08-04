9617a1f29a44226927c59bcbc0f2693285166e6d
Administrator <Administrator@192.168.0.102>
2020-08-04 21:18:58 +0900

    Lab 11-4 commit

---
+++ src/com/example1/User2.java
@@ -0,0 +1,22 @@
+package com.example1;
+
+public class User2 {
+	String hobby;
+	String hobby1;
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	public String getHobby1() {
+		return hobby1;
+	}
+	public void setHobby1(String hobby1) {
+		this.hobby1 = hobby1;
+	}
+	
+
+	
+
+}
